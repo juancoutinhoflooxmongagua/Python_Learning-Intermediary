6bdb6db770f6f82e68c8edb1251eed175162dabc
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-02 15:03:29 -0300

    feat: useful methods in python

--- Functions/exe9.py
+++ Functions/exe9.py
@@ -1,5 +1,5 @@
-pokemons = [
-    {
+pokemons = {
+    'Charmander': {
         'nome': 'Charmander',
         'tipo': 'Fogo',
         'poder': 'Incinerar',
@@ -7,7 +7,7 @@ pokemons = [
         'hp': 39,
         'def': 43
     },
-    {
+    'Bulbasauro': {
         'nome': 'Bulbasauro',
         'tipo': 'Grama',
         'poder': 'Vinhas',
@@ -15,41 +15,45 @@ pokemons = [
         'hp': 45,
         'def': 49
     }
-]
+}
 
-for p in pokemons:
-    if p['nome'] == 'Charmander':
-        my = p
-    if p['nome'] == 'Bulbasauro':
-        enemie = p
 
-if my and enemie:
-    vermelho = '\033[31m'
-    verde = '\033[32m'
-    reset = '\033[0m'   
-    while enemie['hp'] > 0:
-        if my['poder'] == 'Incinerar':
-            input('Pressione ENTER para atacar')
-            print(vermelho, "ATAQUE", my['nome'], my['atk'], reset)
-            print("HP inimigo", enemie['hp'])
-            my['atk_reduzido'] = my['atk'] - enemie['def']
-            print("Pokemon defendeu e o ataque foi reduzido para:", my['atk_reduzido'])
-            print("HP após ataque", enemie['hp'] - my['atk_reduzido'])
+print(pokemons.keys())
+print(list(pokemons.keys()))
 
-        if enemie['poder'] == 'Vinhas':
-            print('UPGRADE', enemie['atk'] )
-            enemie['hp'] = enemie['hp'] + 0.5
-            enemie['atk_reduzido'] = enemie['atk'] - my['def']
-            print("Ataque reduzido para:", enemie['atk_reduzido'])
-            my['hp'] = my['hp'] - enemie['atk_reduzido']
-            print(enemie['nome'], "ATACOU E TIROU A VIDA PARA", my['hp'])
-            print("HP após upgrade", enemie['hp'])
+for name in pokemons:
+    print(name)
 
-        print(verde + "Como estou:", "HP",my['hp'], "ATAQUE",my['atk'], "DEFESA",my['def'], reset)
-        input('Pressione ENTER pra continuar a batalha....')
+"""
+my = pokemons['Charmander']
+enemie = pokemons['Bulbasauro']
 
-        if my['hp'] <= 0:
-            print('VC perdeu')
-            break
+vermelho = '\033[31m'
+verde = '\033[32m'
+reset = '\033[0m'
 
-        
+while enemie['hp'] > 0:
+    if my['poder'] == 'Incinerar':
+        input('Pressione ENTER para atacar')
+        print(vermelho, "ATAQUE", my['nome'], my['atk'], reset)
+        print("HP inimigo", enemie['hp'])
+        my['atk_reduzido'] = my['atk'] - enemie['def']
+        print("Pokemon defendeu e o ataque foi reduzido para:", my['atk_reduzido'])
+        print("HP após ataque", enemie['hp'] - my['atk_reduzido'])
+
+    if enemie['poder'] == 'Vinhas':
+        print('UPGRADE', enemie['atk'])
+        enemie['hp'] = enemie['hp'] + 0.5
+        enemie['atk_reduzido'] = enemie['atk'] - my['def']
+        print("Ataque reduzido para:", enemie['atk_reduzido'])
+        my['hp'] = my['hp'] - enemie['atk_reduzido']
+        print(enemie['nome'], "ATACOU E TIROU A VIDA PARA", my['hp'])
+        print("HP após upgrade", enemie['hp'])
+
+    print(verde + "Como estou:", "HP", my['hp'], "ATAQUE", my['atk'], "DEFESA", my['def'], reset)
+    input('Pressione ENTER pra continuar a batalha....')
+
+    if my['hp'] <= 0:
+        print('VC perdeu')
+        break
+"""
