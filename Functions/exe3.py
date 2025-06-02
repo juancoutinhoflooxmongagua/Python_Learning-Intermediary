0d9d9660ff09099db72a1cc5e77998b981a0c7ce
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-02 10:01:31 -0300

    feat: scope of functions

---
+++ Functions/exe3.py
@@ -0,0 +1,12 @@
+
+local = 'las vegas'
+bairro = 'New Orleans'
+
+def las_vegas():
+    print(f'Estou em {local}')
+    def bairro():
+        print(f'no bairro de {bairro}')
+        bairro()
+
+
+las_vegas()
