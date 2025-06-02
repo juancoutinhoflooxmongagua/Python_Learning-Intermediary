13bb1678be5cccf702d00a4c967605bde010e084
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-02 11:59:04 -0300

    feat: using args

--- Functions/exe4.py
+++ Functions/exe4.py
@@ -3,12 +3,21 @@
 x, y, *_resto = 1, 2, 3, 4
 
 
-def sum(*args):
+def soma(*args):
     total = 0 
     for number in args:
         total += number
-        print(number, total)
+        print(f'Numero somado: {number} igual: {total}')
 
-sum(1,2,3,4)
+soma(1,2,3,4)
 
+n1 = input('primeiro numero: ')
+in1 = int(n1)
+n2 = input('segundo numero: ')
+in2 = int(n2)
+n3 = input('terceiro numero: ')
+in3 = int(n3)
 
+numbers = soma(in1, in2, in3)
+
+print(sum((1,2,3,4)))
