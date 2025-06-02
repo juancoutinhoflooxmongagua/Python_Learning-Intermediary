39dfa0f3272f59f0055c690cc3bb60efa4843ea0
juancoutinhoflooxmongagua <juan.coutinho.flooxmongagua@gmail.com>
2025-06-02 14:53:52 -0300

    feat:testing and studing dictionaries

---
+++ Functions/exe9.py
@@ -0,0 +1,55 @@
+pokemons = [
+    {
+        'nome': 'Charmander',
+        'tipo': 'Fogo',
+        'poder': 'Incinerar',
+        'atk': 52,
+        'hp': 39,
+        'def': 43
+    },
+    {
+        'nome': 'Bulbasauro',
+        'tipo': 'Grama',
+        'poder': 'Vinhas',
+        'atk': 49,
+        'hp': 45,
+        'def': 49
+    }
+]
+
+for p in pokemons:
+    if p['nome'] == 'Charmander':
+        my = p
+    if p['nome'] == 'Bulbasauro':
+        enemie = p
+
+if my and enemie:
+    vermelho = '\033[31m'
+    verde = '\033[32m'
+    reset = '\033[0m'   
+    while enemie['hp'] > 0:
+        if my['poder'] == 'Incinerar':
+            input('Pressione ENTER para atacar')
+            print(vermelho, "ATAQUE", my['nome'], my['atk'], reset)
+            print("HP inimigo", enemie['hp'])
+            my['atk_reduzido'] = my['atk'] - enemie['def']
+            print("Pokemon defendeu e o ataque foi reduzido para:", my['atk_reduzido'])
+            print("HP após ataque", enemie['hp'] - my['atk_reduzido'])
+
+        if enemie['poder'] == 'Vinhas':
+            print('UPGRADE', enemie['atk'] )
+            enemie['hp'] = enemie['hp'] + 0.5
+            enemie['atk_reduzido'] = enemie['atk'] - my['def']
+            print("Ataque reduzido para:", enemie['atk_reduzido'])
+            my['hp'] = my['hp'] - enemie['atk_reduzido']
+            print(enemie['nome'], "ATACOU E TIROU A VIDA PARA", my['hp'])
+            print("HP após upgrade", enemie['hp'])
+
+        print(verde + "Como estou:", "HP",my['hp'], "ATAQUE",my['atk'], "DEFESA",my['def'], reset)
+        input('Pressione ENTER pra continuar a batalha....')
+
+        if my['hp'] <= 0:
+            print('VC perdeu')
+            break
+
+        
